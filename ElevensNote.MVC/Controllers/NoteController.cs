bf2d06ca7ab9b484bf91f4fd4c00d2bba82e96a5
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-28 11:10:45 -0400

    Added Accounts

---
+++ ElevensNote.MVC/Controllers/NoteController.cs
@@ -0,0 +1,25 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevensNote.MVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
